e5696ff13f29908ed1aa440c1248b6915157d2f0
Đinh Văn Thanh <72543449+nguvaichim456@users.noreply.github.com>
2020-12-03 15:22:21 +0700

    Create chieudai.C

---
+++ Bài tập 2/chieudai.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+main(void) {
+	
+  int l,v,t;
+  printf("Nhap do dai can chuyen doi: %d",l);
+  scanf("%d",&l);
+  v=l*100;
+  t=i*100;
+  printf("\Do dai chuyen thanh Xentimet la: %d", v);
+  printf("\DO dai chuyen thanh Milimet la: %d", t);
+}
