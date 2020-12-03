a371e5e5ba6ce75555795c2c8f93aa0e4a8c9dc3
Đinh Văn Thanh <72543449+nguvaichim456@users.noreply.github.com>
2020-12-03 15:42:07 +0700

    Update Tong.c

--- Bài tập 2/Tong.c
+++ Bài tập 2/Tong.c
@@ -1,14 +1,14 @@
-#include  <stdio.h> 
+#include <stdio.h>
 
-int main(void)
-{
-	int a, b;
-	printf("nhap a = ");
-	scanf("%d",&a);
-	printf("nhap b= ");
-	scanf("%d",&b);
-	printf("a + b = %d" ,a + b);
-	return 0;
+
+main(void) {
+	
+  int l,v,t;
+  printf("Nhap do dai can chuyen doi: %d",l);
+  scanf("%d",&l);
+  printf("\Do dai chuyen thanh Xentimet la: %,t", v=l*100);
+  printf("\DO dai chuyen thanh Milimet la: %t", t=v*1000);
+  return 0;
 }
-    
-    
+ 
+   
