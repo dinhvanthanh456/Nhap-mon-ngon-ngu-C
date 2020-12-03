f9b011eaaf896e8cc8090d55dd21705ad9280389
Đinh Văn Thanh <72543449+nguvaichim456@users.noreply.github.com>
2020-12-03 15:43:01 +0700

    Update Tong.c

--- Bài tập 2/Tong.c
+++ Bài tập 2/Tong.c
@@ -6,8 +6,8 @@ main(void) {
   int l,v,t;
   printf("Nhap do dai can chuyen doi: %d",l);
   scanf("%d",&l);
-  printf("\Do dai chuyen thanh Xentimet la: %,t", v=l*100);
-  printf("\DO dai chuyen thanh Milimet la: %t", t=v*1000);
+  printf("Do dai chuyen thanh Xentimet la: %,t", v=l*100);
+  printf("DO dai chuyen thanh Milimet la: %t", t=v*1000);
   return 0;
 }
  
