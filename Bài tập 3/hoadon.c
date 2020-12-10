7e400c3d2428700437b4e55fb192f2a3dc484dfc
Đinh Văn Thanh <72543449+nguvaichim456@users.noreply.github.com>
2020-12-10 16:57:31 +0700

    Create hoadon.c

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    //khai bao bien
+    float (don gian,thanh tien);
+    int (don gia,so luong,thanh tien);
+    printf("nhap vao don gia va so luong");
+    scanf ("%f",&dongia, &soluong);
+    //dieu kien
+    if("soluong >= 5", soluong);
+    getchar();
+    return 0;
+}    
