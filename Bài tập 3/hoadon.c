bfee9f8c395c49eb62bcc97a183be907556874cb
Đinh Văn Thanh <72543449+nguvaichim456@users.noreply.github.com>
2020-12-17 14:40:01 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-	int don gia, so luong, thanh tien;
-    scanf("%d",&don gia);("%d",&so luong);thanh tien=don gia*so luong;
-    if(thanh tien>=500&&don gia>=5){thanh tien=(don gia*so luong)*0.5;
-	printf("%d", thanh tien);
-	}else{printf("%d",thanh tien);}
-	return 0; 
-	}
+int don gia, so luong, thanh tien;
+scanf("%d",&don gia);("%d",&so luong);thanh tien=don gia*so luong;
+if(thanh tien>=500&&don gia>=5){thanh tien=(don gia*so luong)*0.5;
+printf("%d", thanh tien);
+}else{printf("%d",thanh tien);}
+return 0; 
+}
